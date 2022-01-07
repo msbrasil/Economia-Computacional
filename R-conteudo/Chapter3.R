00c53b44e7c62e3314b99be7a39ab996e6c624eb
Matheus <matheusbrasil122@gmail.com>
2022-01-07 15:45:06 -0300

    Revisão
    revisei capitulo 3 e metade do 4

--- R-conteudo/Chapter3.R
+++ R-conteudo/Chapter3.R
@@ -1,19 +1,35 @@
 #Matrices and Arrays
 
-A <- matrix(data=c(-3, 2, 893, 0.17), nrow = 2, ncol = 2) #command to create a matrix
+#### Defining a Matrix ####
+
+A <-
+  matrix(data = c(-3, 2, 893, 0.17),
+         #command to create a matrix
+         nrow = 2,
+         ncol = 2)
 A
 
 ### Filling direction
 
-matrix(data=c(1, 2, 3, 4, 5, 6), nrow=2, ncol=3, byrow = FALSE) #fill the matrix column-by-column. Default is row-by-row
+matrix(
+  data = c(1, 2, 3, 4, 5, 6),
+  nrow = 2,
+  ncol = 3,
+  byrow = FALSE #fill the matrix column-by-column. Default is row-by-row
+)
 
-matrix(data=c(1, 2, 3, 4, 5, 6), nrow=2, ncol=3, byrow = TRUE)
+matrix(
+  data = c(1, 2, 3, 4, 5, 6),
+  nrow = 2,
+  ncol = 3,
+  byrow = TRUE
+)
 
-### Row and column bindings 
+### Row and column bindings
 
-rbind(1:3, 4:6) #create a matrix from 2 vectors or more. r before bind mean bind by row. Only use if the vectors are the same length
+rbind(1:3, 4:6) #create a matrix from 2 vectors or more. r before bind mean bind by row.
 
-cbind(c(1, 4), c(2,5), c(3,6))
+cbind(c(1, 4), c(2, 5), c(3, 6)) #the c before bind mean by column
 
 ### Matrix dimensions
 
@@ -22,92 +38,111 @@ mymat
 
 dim(mymat) #provide the dimensions of matrix stored in your workspace
 
-nrow(mymat)
+nrow(mymat) #rows
 
-ncol(mymat)
+ncol(mymat) #columns
 
-dim(mymat)[2]
+dim(mymat)[2] #second term of dimensions(columns)
 
-## Subsetting
+#### Subsetting ####
 
-A <- matrix(c(0.3, 4.5, 55.3, 91, 0.1, 105.5, -4.2, 8.2, 27.9), nrow = 3, ncol = 3) #matrix 3x3
+A <-
+  matrix(c(0.3, 4.5, 55.3, 91, 0.1, 105.5,-4.2, 8.2, 27.9),
+         nrow = 3,
+         ncol = 3) #matrix 3x3 to make examples
 A
 
-A[3,2] #look the third row and give me the element of second column
+A[3, 2] #look the third row and give me the element of second column
 
 ### Row, Columns and Diagonal Extractions
 
-A[,2] #entire column
+A[, 2] #entire column
 
-A[1,] #entire row
+A[1, ] #entire row
 
-A[2:3,] #row 2 and 3
+A[2:3, ] #row 2 and 3
 
-A[, c(3,1)] #third and second column
+A[, c(3, 1)] #third and second column
 
 A[c(3, 1), 2:3]
 
-diag(x=A) #identify diagonal values of a square matrix starts in A[1, 1]
+diag(x = A) #identify diagonal values of a square matrix starts in A[1, 1]
 
 ### Omitting and Overwriting
 
-A[,-2] #negative indexes delete or omit matrix elements
+A[, -2] #negative indexes delete or omit matrix elements
 
 A[-1, 3:2]
 
-A[-1, -2]
+A[-1,-2]
 
-A[-1, -c(2,3)]
+A[-1,-c(2, 3)]
 
 B <- A
 
-B[2,] <- 1:3 #overwrite second row with sequence 1,2,3
+B[2, ] <- 1:3 #overwrite second row with sequence 1,2,3
 B
 
 B[c(1, 3), 2] <- 900
 B
 
-B[,3] <- B[3,]
+B[, 3] <- B[3, ]
 B
 
 B[c(1, 3), c(1, 3)] <- c(-7, 7)
 B
 
-B[c(1,3), 2:1] <- c(65, -65, 88, -88)
+B[c(1, 3), 2:1] <- c(65,-65, 88,-88)
 B
 
-diag(B) <- rep(0, times=3)
+diag(B) <- rep(0, times = 3)
 B
 
-### Exercise 3.1
+#### Exercise 3.1 ####
 
-#a. 
+#a. Construct and store a 4 × 2 matrix that’s filled row-wise with the
+#values 4.3, 3.1, 8.2, 8.2, 3.2, 0.9, 1.6, and 6.5, in that order.
 
-matrixA <- matrix(c(4.3, 3.1, 8.2, 8.2, 3.2, 0.9, 1.6, 6.5), nrow=4, ncol=2)
-matrixA
 
-#b.
+matrixA <-
+  matrix(
+    c(4.3, 3.1, 8.2, 8.2, 3.2, 0.9, 1.6, 6.5),
+    nrow = 4,
+    ncol = 2,
+    byrow = TRUE
+  )
+matrixA #(correct)
 
-dim(matrixA[-1,])
+#b. Confirm the dimensions of the matrix from (a) are 3 × 2 if you
+#remove any one row.
 
-#c.
+dim(matrixA[-1, ]) #(correct)
 
-matrixA[,2] <- sort(matrixA[,2])
-matrixA
 
-#d.
+#c. Overwrite the second column of the matrix from (a) with that
+#same column sorted from smallest to largest.
 
-matrix(matrixA[-4,-1])
+matrixA[, 2] <- sort(matrixA[, 2])
+matrixA #(correct)
 
-#e.
 
-matrixE <- matrix(matrixA[-c(1,2),], 2, 2)
-matrixE
+#d.What does R return if you delete the fourth row and the first column from (c)?
+#Use matrix to ensure the result is a single-column matrix, rather than a vector.
 
-#f. 
-diag(matrixA)*-1/2 #dafuck
+matrix(matrixA[-4, -1]) #(correct)
 
-## Matrix Operations and Algebra
+#e. Store the bottom four elements of (c) as a new 2 × 2 matrix.
+
+matrixE <- matrix(matrixA[-c(1, 2), ], 2, 2)
+matrixE #(correct)
+
+#f. Overwrite, in this order, the elements of (c) at positions (4,2),
+#(1,2), (4,1), and (1,1) with −1/2 of the two values on the diagonal of (e).
+
+matrixA[c(4, 1), 2:1] <- -0.5 * diag(matrixE)
+matrixA #(correct)
+
+#### Matrix Operations and Algebra ####
 
 ### Matrix Transpose
 
@@ -116,11 +151,11 @@ A
 
 t(A) #transpose function
 
-t(t(A)) 
+t(t(A))
 
-### Indentity Matrix
+### Identity Matrix
 
-A <- diag(x=3)
+A <- diag(x = 3)
 A
 
 ### Scalar Multiple of a Matrix
@@ -128,115 +163,140 @@ A
 A <- rbind(c(2, 5, 2), c(6, 1, 4))
 A
 a <- 2
-a*A
+a * A
 
 ### Matrix Addition and Subtraction
 
 A <- cbind(c(2, 5, 2), c(6, 1, 4))
 A
 
-B <- cbind(c(-2, 3, 6), c(8.1, 8.2, -9.8))
+B <- cbind(c(-2, 3, 6), c(8.1, 8.2,-9.8))
 B
-A-B
+A - B
 
 ### Matrix multiplication
 
 A <- rbind(c(2, 5, 2), c(6, 1, 4))
 dim(A)
-B <- cbind(c(3, -1, 1), c(-3, 1, 5))
+B <- cbind(c(3,-1, 1), c(-3, 1, 5))
 dim(B)
 
-A%*%B
+A %*% B
 
-B%*%A
+B %*% A
 
 ### Matrix inversion
 
-A <- matrix(data=c(3,4,1,2),nrow=2,ncol=2)
+A <- matrix(data = c(3, 4, 1, 2),
+            nrow = 2,
+            ncol = 2)
 A
 
 solve(A)
 
-A%*%solve(A)
+A %*% solve(A)
 
-### Exercise 3.2
+#### Exercise 3.2 ####
 
-#a.
+#a. Calculate the following: (in book)
 
-(2/7) * (matrix(1:6, nrow=3, ncol=2) - matrix(seq(from=10, to=60, by = 10), nrow=3, ncol=2)) 
 
-#b.
+
+A <- cbind(c(1, 2, 7), c(2, 4, 6))
+A
+
+B <- matrix(seq(10, 60, by = 10), 3, 2, byrow = TRUE)
+B
+
+(2 / 7) * (A - B) #(correct)
+
+#b. Store these two matrices: (in book)
 
 A <- matrix(c(1, 2, 7), ncol = 1)
 B <- matrix(c(3, 4, 8), ncol = 1)
 
-A%*%B #not possible
+A %*% B #not possible
 
-t(A)%*%B #possible
+t(A) %*% B #possible (correct)
 
-t(B)%*%(A%*%t(A)) #possible
+t(B) %*% (A %*% t(A)) #possible (correct)
 
-(A%*%t(A))%*%t(B) #not possible
+(A %*% t(A)) %*% t(B) #not possible
 
-solve((B%*%t(B))+(A%*%t(A)) - 100*matrix(diag(x=3))) #not possible
+solve((B %*% t(B)) + (A %*% t(A)) - 100 * matrix(diag(x = 3))) #not possible
 
-#c.
+#c. For (matrix in book) confirm that A−1·A −I4 provides a 4 × 4 matrix of zeros.
 
-A <- diag(x=c(2, 3, 5, -1))
+A <- diag(x = c(2, 3, 5,-1))
 A
 
-solve(A) %*% A - diag(x=4)
+solve(A) %*% A - diag(x = 4) #(correct)
 
-## Multidimensional Arrays
+#### Multidimensional Arrays ####
 
 ### Definition
 
-AR <- array(data = 1:24, dim=c(3,4,2)) #dim argument: rows, columns, layers
+AR <-
+  array(data = 1:24, dim = c(3, 4, 2)) #dim argument: rows, columns, layers
 AR
 
-BR <- array(data=rep(1:24, times=3), dim=c(3,4,2,3)) #row, column, layer, block
+BR <-
+  array(data = rep(1:24, times = 3), dim = c(3, 4, 2, 3)) #row, column, layer, block
 BR
 
 ### Subsets, Extractions, and Replacements
 
-AR[2,,2]
+AR[2, , 2]
 
-AR[2,c(3,1),2]
+AR[2, c(3, 1), 2]
 
-AR[1,,]
+AR[1, , ]
 
 BR[2, 1, 1, 3]
 
-BR[1,,,1]
+BR[1, , , 1]
 
-BR[,,2,]
+BR[, , 2, ]
 
-BR[3:2, 4,,]
+BR[3:2, 4, , ]
 
-BR[2,,1,]
+BR[2, , 1, ]
 
-# Exercise 3.3
+#### Exercise 3.3 ####
 
-#a.
+#a.Create and store a three-dimensional array with six layers of a
+#4 × 2 matrix, filled with a decreasing sequence of values between
+#4.8 and 0.1 of the appropriate length.
 
-arrayA <- array(seq(from=4.8, to=0.1, length.out=144), dim=c(4, 2, 6, 3))
-arrayA
+arrayA <-
+  array(seq(
+    from = 4.8,
+    to = 0.1,
+    length.out = 48
+  ), dim = c(4, 2, 6))
+arrayA #(correct)
 
-#b.
+#b. Extract and store as a new object the fourth- and first-row elements, 
+#in that order, of the second column only of all layers of (a).
 
-extractA <- arrayA[c(4,1), 2,,]
-extractA
 
-#c.
+extractA <- arrayA[c(4, 1), 2, ]
+extractA #(correct)
 
-matrixC <- array(rep(extractA[2], times=4), dim=c(2,2,2,3))
-matrixC
-#d.
+#c.Use a fourfold repetition of the second row of the matrix formed
+#in (b) to fill a new array of dimensions 2 × 2 × 2 × 3.
 
-arrayD <- arrayA[,,-6,]
-arrayD
+matrixC <- array(rep(extractA[2,], times = 4), dim = c(2, 2, 2, 3))
+matrixC #(correct)
 
-#e.
+#d.  Create a new array comprised of the results of deleting the sixth
+#layer of (a).
 
-arrayD[c(2, 4), 2, c(1,3,5),] <- -99
-arrayD
+arrayD <- arrayA[, , -6]
+arrayD #(correct)
+
+#e. Overwrite the second and fourth row elements of the second
+#column of layers 1, 3, and 5 of (d) with −99.
+
+arrayD[c(2, 4), 2, c(1, 3, 5), ] <- -99
+arrayD #(correct)
--- R-conteudo/Chapter4.R
+++ R-conteudo/Chapter4.R
@@ -1,250 +1,307 @@
 # Non-numeric values
 
-## Logical Values
+#### Logical Values ####
 
 ### TRUE or FALSE?
 
 foo <- TRUE
 foo
 
-bar <- F
+bar <- F #just the first letter works too
 bar
 
-baz <- c(T, F, F, F, T, F, T, T, T, F, T, F) #vectors can be filled with logical values
+baz <-
+  c(T, F, F, F, T, F, T, T, T, F, T, F) #vectors can be filled with logical values
 baz
 
 length(baz)
 
-qux <- matrix(baz, nrow=3, ncol=4, byrow=foo)
+qux <- matrix(baz,
+              nrow = 3,
+              ncol = 4,
+              byrow = foo)
 qux
 
 ### A Logical Outcome: Relational Operators
 
-1==2
+1 == 2 #equal...
 
-1>2
+1 > 2 #more than
 
-(2-1)<=2
+(2 - 1) <= 2 #more or equal than... 
 
-1!=(2+3)
+1 != (2 + 3) #different 
 
-foo <- c(3,2,1,4,1,2,1,-1,0,3)
-bar <- c(4,1,2,1,1,0,0,3,0,4)
+foo <- c(3, 2, 1, 4, 1, 2, 1, -1, 0, 3)
+bar <- c(4, 1, 2, 1, 1, 0, 0, 3, 0, 4)
 
-length(foo)==length(bar)
+length(foo) == length(bar)
 
-foo==bar
+foo == bar #compare each item in vector
 
-foo<bar
+foo < bar
 
-foo<=bar
+foo <= bar
 
-foo<=(bar+10)
+foo <= (bar + 10)
 
-baz <- foo[c(10,3)]
+baz <- foo[c(10, 3)]
 baz
 
-foo>baz
+foo > baz
 
-foo<3
+foo < 3 #compare each value is less than 3
 
-foo.mat <- matrix(foo,nrow=5, ncol=2)
+foo.mat <- matrix(foo, nrow = 5, ncol = 2)
 foo.mat
 
-bar.mat <- matrix(bar,nrow=5, ncol=2)
+bar.mat <- matrix(bar, nrow = 5, ncol = 2)
 bar.mat
 
-foo.mat<=bar.mat
+foo.mat <= bar.mat
 
-foo.mat<3
+foo.mat < 3
 
-qux <- foo==bar
+qux <- foo == bar
 qux
 
-any(qux) #any() returns TRUE if any of the logical values are TRUE and FALSE otherwise 
+any(qux) #any() returns TRUE if any of the logical values are TRUE and FALSE otherwise
 
 all(qux) #all() returns TRUE only if all logical values are TRUE.
 
-quux <- foo<=(bar+10)
+quux <- foo <= (bar + 10)
 quux
 
 any(quux)
 
 all(quux)
 
-# Exercise 4.1
+#### Exercise 4.1 ####
 
-#a. 
+#a.Store the following vector of 15 values as an object in your
+#workspace: c(6,9,7,3,6,7,9,6,3,6,6,7,1,9,1). Identify the following elements:
+#  i. Those equal to 6
+#ii. Those greater than or equal to 6
+#iii. Those less than 6 + 2
+#iv. Those not equal to 6
 
-vectorA <- c(6,9,7,3,6,7,9,6,3,6,6,7,1,9,1)
+vectorA <- c(6, 9, 7, 3, 6, 7, 9, 6, 3, 6, 6, 7, 1, 9, 1)
 
-vectorA==6
-vectorA>=6
-vectorA<6+2
-vectorA!=6
+vectorA == 6
+vectorA >= 6
+vectorA < 6 + 2
+vectorA != 6
+#(correct)
 
-#b.
+#b. Create a new vector from the one used in (a) by deleting its
+#first three elements. With this new vector, fill a 2 × 2 × 3 array.
+#Examine the array for the following entries:
+#  i. Those less than or equal to 6 divided by 2, plus 4
+#ii. Those less than or equal to 6 divided by 2, plus 4, after
+#increasing every element in the array by 2
 
-vectorB <- vectorA[-c(1,2,3)]
-arrayB <- array(vectorB, dim=c(2,2,3))
+vectorB <- vectorA[-c(1, 2, 3)]
+arrayB <- array(vectorB, dim = c(2, 2, 3))
 
-arrayB <= 6/2+4
-arrayB+2<= 6/2+4
+arrayB <= (6 / 2 + 4) #(correct)
+(arrayB + 2) <= (6 / 2 + 4) #(correct)
 
-#c.
+#c.Confirm the specific locations of elements equal to 0 in the
+#10 × 10 identity matrix I10 (see Section 3.3).
 
-matrixC <- diag(x=10)
-matrixC==0
+matrixC <- diag(x = 10)
+matrixC == 0 #(correct)
 
-#d.
+#d. Check whether any of the values of the logical arrays created in
+#(b) are TRUE. If they are, check whether they are all TRUE.
 
-any(arrayB)
+any(bar <= (6 / 2 + 4))
+all(bar <= (6 / 2 + 4))
+any((bar + 2) <= (6 / 2 + 4))
+all((bar + 2) <= (6 / 2 + 4))
+#(correct)
 
-all(arrayB)
+#e. By extracting the diagonal elements of the logical matrix created
+#in (c), use any to confirm there are no TRUE entries.
 
-#e.
-
-extractC <- matrixC[diag()]
-
-#how extract the diagonal elements?
+any(diag(diag(10) == 0)) #(correct)
 
 ### Multiple Comparisons: Logical Operators
 
-FALSE || ((T&&TRUE) || FALSE)
+FALSE || ((T && TRUE) || FALSE)
 
-!TRUE&&TRUE
+! TRUE && TRUE
 
-(T&&(TRUE||F))&&FALSE
+(T && (TRUE || F)) && FALSE
 
-(6<4)||(3!=1)
+(6 < 4) || (3 != 1)
 
 foo <- c(T, F, F, F, T, F, T, T, T, F, T, F)
 bar <- c(F, T, F, T, F, F, F, F, T, T, T, T)
 
-foo&bar
+foo & bar
 
 foo | bar
 
-foo&&bar
+foo && bar
 
-foo||bar
+foo || bar
 
-# Exercise 4.2
+#### Exercise 4.2 ####
 
-#a.
+#a. Store the vector c(7,1,7,10,5,9,10,3,10,8) as foo. Identify the
+#elements greater than 5 OR equal to 2.
 
 foo <- c(7, 1, 7, 10, 5, 9, 10, 3, 10, 8)
 
-foo > 5 | foo == 2
+(foo > 5) | (foo == 2) #(correct)
 
-#b.
+#b. Store the vector c(8,8,4,4,5,1,5,6,6,8) as bar. 
+#Identify the elements less than or equal to 6 AND not equal to 4.
 
 bar <- c(8, 8, 4, 4, 5, 1, 5, 6, 6, 8)
 
-bar <= 6 & bar != 4
+(bar <= 6) & (bar != 4) #(correct)
 
-#c.
+#c.Identify the elements that satisfy (a) in foo AND satisfy (b) in bar.
 
-(foo > 5 | foo == 2) & (bar <= 6 & bar != 4)
+((foo > 5) | (foo == 2)) & ((bar <= 6) & (bar != 4)) #correct
 
-#d. 
+#d. Store a third vector called baz that is equal to the element-wise
+#sum of foo and bar. Determine the following:
+#  i. The elements of baz greater than or equal to 14 but not equal to 15
+#ii. The elements of the vector obtained via an element-wise
+#division of baz by foo that are greater than 4 OR less than or equal to 2
 
 baz <- foo + bar
 baz
 
-baz >= 14 & baz != 15
+(baz >= 14) & (baz != 15)
 
-baz/foo > 4 | baz/foo <= 2
+(baz / foo > 4) | (baz / foo <= 2)
 
-#e. ?
+#e.  Confirm that using the long version in all of the preceding
+#exercises performs only the first comparison (that is, the results
+#each match the first entries of the previously obtained vectors).
+
+(foo>5)||(foo==2)
+(bar<=6)&&(bar!=4)
+((foo>5)||(foo==2))&&((bar<=6)&&(bar!=4))
+(baz>=14)&&(baz!=15)
+(baz/foo>4)||(baz/foo<=2)
 
 ### Logicals Are Numbers!
 
-TRUE + TRUE
+TRUE + TRUE #TRUE equals 1
 
-FALSE - TRUE
+FALSE - TRUE #FALSE equals 0
 
-T+T+F+T+F+F+T
+T + T + F + T + F + F + T
 
-1&&1
+1 && 1
 
-1||0
+1 || 0
 
-0&&1
+0 && 1
 
 ### logical Subsetting and Extraction
 
-myvec <- c(5, -2.3, 4, 4, 4, 6, 8, 10, 40221, -8)
+myvec <- c(5,-2.3, 4, 4, 4, 6, 8, 10, 40221,-8)
 
-myvec[c(F,T,F,F,F,F,F,F,F,T)] #extract negative values
+myvec[c(F, T, F, F, F, F, F, F, F, T)] #extract negative values
 
 myvec < 0
 
-myvec[myvec<0]
+myvec[myvec < 0]
 
 myvec[c(T, F)]
 
-myvec[(myvec>0)&(myvec<1000)]
+myvec[(myvec > 0) & (myvec < 1000)]
 
-myvec[myvec<0] <- -200
+myvec[myvec < 0] <- -200
 myvec
 
-which(x=c(T, F, F, T, T)) #returns the indexes corresponding to the positions of all TRUE entries
+which(x = c(T, F, F, T, T)) #returns the indexes corresponding to the positions of all TRUE entries
 
-which(x=myvec<0)
+which(x = myvec < 0)
 
-myvec[-which(x=myvec<0)] #omit negative entries of myvec
+myvec[-which(x = myvec < 0)] #omit negative entries of myvec
 
-A <- matrix(c(0.3,4.5,55.3,91,0.1,105.5,-4.2,8.2,27.9),nrow=3,ncol=3)
+A <-
+  matrix(c(0.3, 4.5, 55.3, 91, 0.1, 105.5, -4.2, 8.2, 27.9),
+         nrow = 3,
+         ncol = 3)
 A
 
 A[c(T, F, F), c(F, T, T)]
 
-A<1
+A < 1
 
-A[A<1] <- -7
+A[A < 1] <- -7
 A
 
 A > 25
 
-which(A>25) #returns de indexes by stacking the columns
+which(A > 25) #returns de indexes by stacking the columns
 
-which(x=c(A[,1],A[,2],A[,3])>25)
+which(x = c(A[, 1], A[, 2], A[, 3]) > 25)
 
-which(x=A>25, arr.ind = T) #easier way to see all entries positions
+which(x = A > 25, arr.ind = T) #easier way to see all entries positions
 
-# Exercise 4.3
+#### Exercise 4.3 ####
 
-#a. 
+#a.  Store this vector of 10 values: foo <- c(7,5,6,1,2,10,8,3,8,2).
+#Then, do the following:
+# i. Extract the elements greater than or equal to 5, storing the result as bar.
+#ii. Display the vector containing those elements from foo that
+#remain after omitting all elements that are greater than or equal to 5.
+
 foo <- c(7, 5, 6, 1, 2, 10, 8, 3, 8, 2)
 
-bar <- foo[foo>=5]
+bar <- foo[foo >= 5] #correct
 bar
 
-foo[-which(foo>=5)]
+foo[-which(foo >= 5)] #correct
 
-#b.
+#b. Use bar from (a)(i) to construct a 2 × 3 matrix called baz, filled in
+#a row-wise fashion. Then, do the following:
+# i. Replace any elements that are equal to 8 with the squared
+#value of the element in row 1, column 2 of baz itself.
+#ii. Confirm that all values in baz are now less than or equal to 25
+#AND greater than 4.
 
-baz = matrix(bar, 2, 3, byrow = T)
+baz = matrix(bar, 2, 3, byrow = T) #correct
 baz
 
-baz[baz==8] <- (baz[1,2])**2
+baz[baz == 8] <- (baz[1, 2]) ** 2 #correct
 baz
 
-baz[(baz<=25)&(baz>4)]
+all(baz<=25&baz>4) #correct
+#c.  Create a 3 × 2 × 3 array called qux using the following vector of
+#18 values: c(10,5,1,4,7,4,3,3,1,3,4,3,1,7,8,3,7,3). Then, do the following:
+#  i. Identify the dimension-specific index positions of elements
+#that are either 3 OR 4.
+#ii. Replace all elements in qux that are less than 3 OR greater
+#than or equal to 7 with the value 100.
 
-#c.
-
-qux <- array(c(10,5,1,4,7,4,3,3,1,3,4,3,1,7,8,3,7,3), dim = c(3,2,3))
+qux <-
+  array(c(10, 5, 1, 4, 7, 4, 3, 3, 1, 3, 4, 3, 1, 7, 8, 3, 7, 3), dim = c(3, 2, 3))
 qux
 
-which(x=qux==4, arr.ind = T)
+which(x = qux == 3 | qux == 4, arr.ind = T)
 
-#d.
+qux[qux<3|qux>=7] <- 100
+qux
+#d. Return to foo from (a). Use the vector c(F,T) to extract every
+#second value from foo. In Section 4.1.4, you saw that in some
+#situations, you can substitute 0 and 1 for TRUE and FALSE. Can you
+#perform the same extraction from foo using the vector c(0,1)?
+#Why or why not? What does R return in this case?
 
 foo[c(F, T)]
+foo[c(0, 1)]
 
-## Characters
+#### Characters ####
 
 ### Creating a String
 
@@ -256,47 +313,48 @@ nchar(foo) #count individual number of characters
 bar <- "23.3"
 bar
 
-bar*2
+bar * 2
 
-'alpha'=='alpha'
-'alpha'!='beta'
-c('alpha','beta','gamma')=='beta'
+'alpha' == 'alpha'
+'alpha' != 'beta'
+c('alpha', 'beta', 'gamma') == 'beta'
 
-'alpha'<='beta'
-'gamma'>'Alpha'
+'alpha' <= 'beta'
+'gamma' > 'Alpha'
 
-'Alpha'>'alpha'
-'beta'>='bEtA'
+'Alpha' > 'alpha'
+'beta' >= 'bEtA'
 
 baz <- "&4 _ 3 **%.? $ymbolic non$en$e ,; "
 baz
 
 ### Concatenation
 
-qux <- c('awesome','R','is')
+qux <- c('awesome', 'R', 'is')
 length(qux)
 qux
 
-cat(qux[2], qux[3],'totally',qux[1],'!') #output direcly to the console
+cat(qux[2], qux[3], 'totally', qux[1], '!') #output direcly to the console
 
-paste(qux[2], qux[3],'totally',qux[1],'!') #returns a usable R object
+paste(qux[2], qux[3], 'totally', qux[1], '!') #returns a usable R object
 
-paste(qux[2], qux[3],'totally',qux[1],'!', sep='---')
-paste(qux[2], qux[3],'totally',qux[1],'!', sep = '')
+paste(qux[2], qux[3], 'totally', qux[1], '!', sep = '---')
+paste(qux[2], qux[3], 'totally', qux[1], '!', sep = '')
 
 
-cat("Do you think ",qux[2]," ",qux[3]," ",qux[1],"?",sep="")
+cat("Do you think ", qux[2], " ", qux[3], " ", qux[1], "?", sep = "")
 
 a <- 3
 b <- 4.4
 
-cat("The value stored as 'a' is ",a,".",sep="") #coerce these itens into strings
+cat("The value stored as 'a' is ", a, ".", sep = "") #coerce these itens into strings
 
-paste("The value stored as 'b' is ",b,".",sep="")
+paste("The value stored as 'b' is ", b, ".", sep = "")
 
-cat("The result of a+b is ",a,"+",b,"=",a+b,".",sep="")
+cat("The result of a+b is ", a, "+", b, "=", a + b, ".", sep = "")
 
-paste("Is ",a+b," less than 10? That's totally ",a+b<10,".",sep="")
+paste("Is ", a + b, " less than 10? That's totally ", a + b < 10, ".", sep =
+        "")
 
 ### Escape Sequences
 
@@ -312,25 +370,29 @@ cat("here is a string\nsplit\tto new\b\n\n\tlines")
 
 cat("I really want a backslash: \\\nand a double quote: \"")
 
-setwd("/folder1/folder2/folder3/") #folder separate must use slash 
+setwd("/folder1/folder2/folder3/") #folder separate must use slash
 
 ### Substrings and Matching
 
 foo <- "This is a character string!"
-substr(x=foo,start=21,stop=27)
+substr(x = foo, start = 21, stop = 27)
 
-substr(x=foo,start=1,stop=4) <- 'Here'
+substr(x = foo, start = 1, stop = 4) <- 'Here'
 foo
 
-bar <- "How much wood could a woodchuck chuck"     
+bar <- "How much wood could a woodchuck chuck"
 
-sub(pattern="chuck",replacement="hurl",x=bar) #search a smaller string pattern
+sub(pattern = "chuck",
+    replacement = "hurl",
+    x = bar) #search a smaller string pattern
 
-gsub(pattern="chuck",replacement="hurl",x=bar) #search every istance
+gsub(pattern = "chuck",
+     replacement = "hurl",
+     x = bar) #search every istance
 
-# Exercise 4.4
+#### Exercise 4.4 ####
 
-#a. 
+#a.
 
 cat("the quick brown fox\njumped over\n\tthe lazy dogs")
 
@@ -339,7 +401,7 @@ cat("the quick brown fox\njumped over\n\tthe lazy dogs")
 num1 <- 4
 num2 <- 0.75
 
-cat('The result of multiplying', num1,'by', num2,'is',num1*num2)
+cat('The result of multiplying', num1, 'by', num2, 'is', num1 * num2)
 
 #c.
 
@@ -348,26 +410,39 @@ cat('The result of multiplying', num1,'by', num2,'is',num1*num2)
 #d.
 
 bar <- "if a woodchuck could chuck wood"
-gsub(pattern='wood', replacement = 'metal',bar)
+gsub(pattern = 'wood', replacement = 'metal', bar)
 
 #e.
 
-## Factors
+#### Factors ####
 
 ### Identifying Categories
 
-firstname <- c("Liz", "Jolene", "Susan", "Boris", "Rochelle", "Tim",
-               "Simon", "Amy")
+firstname <- c("Liz",
+               "Jolene",
+               "Susan",
+               "Boris",
+               "Rochelle",
+               "Tim",
+               "Simon",
+               "Amy")
 
 sex.num <- c(0, 0, 0, 1, 0, 1, 1, 0)
 
-sex.char <- c("female", "female", "female", "male", "female", "male",
-              "male", "female")
+sex.char <-
+  c("female",
+    "female",
+    "female",
+    "male",
+    "female",
+    "male",
+    "male",
+    "female")
 
-sex.num.fac <- factor(x=sex.num) #create a factor variable.
+sex.num.fac <- factor(x = sex.num) #create a factor variable.
 sex.num.fac
 
-sex.char.fac <- factor(x=sex.char)
+sex.char.fac <- factor(x = sex.char)
 sex.char.fac
 
 levels(sex.char.fac) #extract the levels from a factor
@@ -381,7 +456,7 @@ sex.char.fac[c(1:3, 5, 8)]
 
 sex.num.fac == '2'
 
-firstname[sex.char.fac=='male']
+firstname[sex.char.fac == 'male']
 
 ### Defining and ordering Levels
 
@@ -389,16 +464,29 @@ mob <- c("Apr", "Jan", "Dec", "Sep", "Nov", "Jul", "Jul", "Jun")
 
 mob[2]
 mob[3]
-mob[2]<mob[3]
+mob[2] < mob[3]
 
-ms <- c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov",
-        "Dec")
-mob.fac <- factor(x=mob, levels = ms, ordered = T)
+ms <-
+  c("Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec")
+mob.fac <- factor(x = mob,
+                  levels = ms,
+                  ordered = T)
 mob.fac
 
 mob.fac[2]
 mob.fac[3]
-mob.fac[2]<mob.fac[3]
+mob.fac[2] < mob.fac[3]
 
 ### Combining and Cutting
 
@@ -406,14 +494,20 @@ foo <- c(5.1, 3.3, 3.1, 4)
 bar <- c(4.5, 1.2)
 c(foo, bar) #this dont work with factor-valued vectors
 
-new.values <- factor(x=c('Oct', 'Feb','Feb'), levels = levels(mob.fac),
-                     ordered = T)
+new.values <-
+  factor(
+    x = c('Oct', 'Feb', 'Feb'),
+    levels = levels(mob.fac),
+    ordered = T
+  )
 new.values
 
 c(mob.fac, new.values)
 
 mob.new <- levels(mob.fac)[c(mob.fac, new.values)]
-mob.new.fac <- factor(x=mob.new, levels=levels(mob.fac), ordered=T)
+mob.new.fac <- factor(x = mob.new,
+                      levels = levels(mob.fac),
+                      ordered = T)
 mob.new.fac
 
 Y <- c(0.53, 5.4, 1.5, 3.33, 0.45, 0.01, 2, 4.2, 1.99, 1.01)
@@ -421,9 +515,15 @@ br <- c(0, 2, 4, 6)
 
 lab <- c('Small', 'Medium', 'Large')
 
-cut(x=Y, breaks = br, right = F, include.lowest = T, labels = lab)
+cut(
+  x = Y,
+  breaks = br,
+  right = F,
+  include.lowest = T,
+  labels = lab
+)
 
-# Exercise 4.5
+#### Exercise 4.5 ####
 
 #a.
 
@@ -441,27 +541,31 @@ nz.party
 
 #b.
 
-nz.sex.fac <- factor(x=nz.sex, levels=(c('M', 'F')))
+nz.sex.fac <- factor(x = nz.sex, levels = (c('M', 'F')))
 nz.sex.fac
 
-nz.party.fac <- factor(x=nz.party, levels=c('National','Labour','Greens','Maori','Other'))
+nz.party.fac <-
+  factor(x = nz.party,
+         levels = c('National', 'Labour', 'Greens', 'Maori', 'Other'))
 nz.party.fac
 
 #c.
 
-nz.party.fac[nz.sex.fac=='M']
+nz.party.fac[nz.sex.fac == 'M']
 
-nz.sex.fac[nz.party.fac=='National']
+nz.sex.fac[nz.party.fac == 'National']
 
 #d.
 
-new.survey.sex <- factor(x=c("M","M","F","F","F","M"), 
+new.survey.sex <- factor(x = c("M", "M", "F", "F", "F", "M"),
                          levels = levels(nz.sex.fac))
 new.survey.sex
 
-new.survey.party <- factor(x=c("National","Maori","Maori",
-                               "Labour","Greens","Labour"), 
-                              levels = levels(nz.party.fac))
+new.survey.party <- factor(
+  x = c("National", "Maori", "Maori",
+        "Labour", "Greens", "Labour"),
+  levels = levels(nz.party.fac)
+)
 new.survey.party
 
 nz.sex.fac <- c(nz.sex.fac, new.survey.sex)
@@ -472,15 +576,45 @@ nz.party.fac
 
 #e.
 
-lab <- ('Low','Moderate','High')
+lab <- ('Low', 'Moderate', 'High')
 br <- c(0, 30, 70, 100)
 
-conf.percent <- c(93, 55, 29, 100,52, 84, 56, 0, 
-                  33, 52, 35, 53, 55, 46, 40, 40, 
-                  56, 45, 64, 31, 10, 29, 40, 95, 18, 61)
+conf.percent <- c(93,
+                  55,
+                  29,
+                  100,
+                  52,
+                  84,
+                  56,
+                  0,
+                  33,
+                  52,
+                  35,
+                  53,
+                  55,
+                  46,
+                  40,
+                  40,
+                  56,
+                  45,
+                  64,
+                  31,
+                  10,
+                  29,
+                  40,
+                  95,
+                  18,
+                  61)
 
 #f.
 
-conf.percent.fac <- cut(x=conf.percent, breaks = br, right = T, include.lowest = T, labels = lab)
-conf.percent.fac[nz.party.fac=='Labour']
-conf.percent.fac[nz.party.fac=='National']
+conf.percent.fac <-
+  cut(
+    x = conf.percent,
+    breaks = br,
+    right = T,
+    include.lowest = T,
+    labels = lab
+  )
+conf.percent.fac[nz.party.fac == 'Labour']
+conf.percent.fac[nz.party.fac == 'National']
