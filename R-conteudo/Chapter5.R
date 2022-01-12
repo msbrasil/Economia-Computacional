34929e15a32a717f1f142410cd16dcde30046d7b
Matheus <matheusbrasil122@gmail.com>
2022-01-11 23:51:16 -0300

    Revisão
    Revisei conteúdo do capítulo 5 e primeira parte do capítulo 6 sobre objetos especiais

--- R-conteudo/Chapter5.R
+++ R-conteudo/Chapter5.R
@@ -1,15 +1,16 @@
 #Lists and Dataframes
 
-## Lists of Objects
+#### Lists of Objects ####
 
 ### Definition and Component Access
 
 foo <- list(matrix(data = 1:4, nrow=2, ncol=2), c(T,F,T,T), 'hello')
 foo
+
 length(foo)
 foo[3]
 
-foo[[1]] + 5.5
+foo[[1]] + 5.5 #subsetting a list
 
 cat(foo[[3]], 'you!')
 
@@ -20,40 +21,96 @@ foo
 
 foo$mymatrix
 
+#creating a list with names already 
 baz <- list(tom=c(foo[[2]],T,T,T,F),dick="g'day mate",harry=foo$mymatrix*2)
 baz
 
-names(baz)
+names(baz) #how to see the name of objects
 
 ### Nesting
 
-baz$bobby <- foo
+baz$bobby <- foo #lists can have lists
 baz
 
-# Exercise 5.1
+baz$bobby$mylogicals #seeing an object in list at list
 
-#a.
+#### Exercise 5.1 ####
+
+# a.Create a list that contains, in this order, a sequence of 20 evenly
+# spaced numbers between −4 and 4; a 3 × 3 matrix of the logical
+# vector c(F,T,T,T,F,T,T,F,F) filled column-wise; a character vector
+# with the two strings "don" and "quixote"; and a factor vector containing 
+# the observations c("LOW","MED","LOW","MED","MED","HIGH").
+# Then, do the following:
+#   i. Extract row elements 2 and 1 of columns 2 and 3, in that
+# order, of the logical matrix.
+# ii. Use sub to overwrite "quixote" with "Quixote" and "don" with
+# "Don" inside the list. Then, using the newly overwritten list
+# member, concatenate to the console screen the following
+# statement exactly: (in book)
+# iii. Obtain all values from the sequence between −4 and 4 that
+# are greater than 1.
+# iv. Using which, determine which indexes in the factor vector are
+# assigned the "MED" level.
 
 list.a <- list(seq(from=-4, to=4, length.out=20), 
                matrix(c(F,T,T,T,F,T,T,F,F), nrow=3, ncol=3), 
                c('don','quixote'), 
                factor(x=c("LOW","MED","LOW","MED","MED","HIGH"), 
                               levels = c('LOW','MED','HIGH')))
-list.a
+list.a #correct
 
-list.a <- sub(pattern = 'don', list.a, replacement = 'Don')
-list.a <- sub(pattern = 'quixote', list.a, replacement = 'Quixote')
-list.a
 
-cat("WIndmills! ATTACK! \n \t -\\",list.a[3[1]],list.a[3[2]],"/-")
+list.a[[3]][1] <- sub(pattern = 'd', replacement = 'D', x = list.a[[3]][1])
+list.a[[3]][2] <- sub(pattern = 'q', replacement = 'Q', x = list.a[[3]][2])
+list.a #correct
 
-#### Can do it rigth now. Finish the exercises later.
+cat("WIndmills! ATTACK! \n \t -\\",list.a[[3]][1],list.a[[3]][2],"/-") #correct
+ 
+# b.Create a new list with the factor vector from (a) as a component named "facs"; 
+# the numeric vector c(3,2.1,3.3,4,1.5,4.9) as a
+# component named "nums"; and a nested list comprised of the first
+# three members of the list from (a) (use list slicing to obtain this),
+# named "oldlist". Then, do the following:
+#   i. Extract the elements of "facs" that correspond to elements of
+# "nums" that are greater than or equal to 3.
+# ii. Add a new member to the list named "flags". This member
+# should be a logical vector of length 6, obtained as a twofold
+# repetition of the third column of the logical matrix in the
+# "oldlist" component.
+# iii. Use "flags" and the logical negation operator ! to extract the
+# entries of "num" corresponding to FALSE.
+# iv. Overwrite the character string vector component of "oldlist"
+# with the single character string "Don Quixote".
 
-## Data Frames
+newlist <- list(facs = list.a[[4]], 
+                num = c(3,2.1,3.3,4,1.5,4.9),
+                oldlist = list.a[1:3])
+newlist #correct
+#i.
+
+newlist$facs[newlist$num>=3] #correct
+
+#ii.
+newlist$facs
+
+newlist$flags <- c(rep(newlist$oldlist[[2]][,3], time=2))
+newlist$flags #correct
+
+#iii.
+
+newlist$num[!newlist$flags] #correct
+
+#iv.
+
+newlist$oldlist[[3]] <- "Don Quixote" 
+newlist$oldlist[[3]] #correct
+
+#### Data Frames ####
 
 ### Construction
 
-#to create a dataframe from scrath, use the  data.frame function
+#to create a data frame from scratch, use the  data.frame function
 mydata <- data.frame(person=c("Peter","Lois","Meg","Chris","Stewie"), 
                      age=c(42,40,17,14,1),
                      sex=factor(c("M","F","F","M","M")))
@@ -71,9 +128,9 @@ mydata$age[2]
 
 #reporting the size of a data frame
 
-nrow(mydata)
-ncol(mydata)
-dim(mydata)
+nrow(mydata) #just rows
+ncol(mydata) #just columns
+dim(mydata)  #rows and columns
 
 mydata$person
 
@@ -121,42 +178,63 @@ mydata[mydata$age>10|mydata$funny=='High',] #more than 10y old OR high funny deg
 
 mydata[mydata$age>45,]
 
-### Exercise 5.2
+#### Exercise 5.2 ####
 
-#a.
+# a. Create and store this data frame as dframe in your R workspace: (in book)
+# The variables person, sex, and funny should be identical in
+# nature to the variables in the mydata object studied throughout
+# Section 5.2. That is, person should be a character vector, sex
+# should be a factor with levels F and M, and funny should be a
+# factor with levels Low, Med, and High.
 
-df.a <- data.frame(person=c("Stan","Francine","Steve","Roger","Hayley","Klaus"),
+dframe <- data.frame(person=c("Stan","Francine","Steve","Roger","Hayley","Klaus"),
                    sex=factor(c("M","F","M","M","F","M")),
                    funny=factor(c("High","Med","Low","High","Med","Med")))
-df.a
-df.a$sex                   
-df.a$person
+dframe      #correct
+dframe$sex  #correct              
+dframe$person #correct
 
-#b.
 
-age <- c(41, 41, 15, 21, 60, 1600)
+# b. Stan and Francine are 41 years old, Steve is 15, Hayley is 21, and
+# Klaus is 60. Roger is extremely old—1,600 years. Append these
+# data as a new numeric column variable in dframe called age.
 
-df.a <- cbind(df.a, age)
-df.a
+dframe$age <- c(41,41,15,1600,21,60)
+dframe #correct
 
-#c.
+# c.Use your knowledge of reordering the column variables based
+# on column index positions to overwrite dframe, bringing it in line
+# with mydata. That is, the first column should be person, the second
+# column age, the third column sex, and the fourth column funny.
 
-df.a[,c(1,2,3,4)] <- df.a[,c(1,4,2,3)]
-df.a
-names(df.a) <- c('person','age','sex','funny')
-df.a  
+dframe[,c(1,2,3,4)] <- dframe[,c(1,4,2,3)]
+dframe
+names(dframe) <- c('person','age','sex','funny')
+dframe  
 
-d.
+# d. Turn your attention to mydata as it was left after you included the
+# age.mon variable in Section 5.2.2. Create a new version of mydata
+# called mydata2 by deleting the age.mon column. 
+
 mydata
 mydata2 <- mydata[,-5]
-mydata2
+mydata2 #correct
 
-mydataframe <- rbind(df.a, mydata2)
+# e.  Now, combine mydata2 with dframe, naming the resulting object
+# mydataframe.
 
-#f.
+mydataframe <- rbind(dframe, mydata2)
+mydataframe #correct
 
-mydataframe[mydataframe$sex=='F'&mydataframe$funny=='High'|mydataframe$funny=='Med',]
+# f.Write a single line of code that will extract from mydataframe just
+# the names and ages of any records where the individual is female
+# and has a level of funniness equal to Med OR High.
 
-#g.
+mydataframe[mydataframe$sex=='F'&(mydataframe$funny=='High'|mydataframe$funny=='Med'),c("person","age")]
+#correct
 
-mydataframe[substr(mydataframe$person, start = 1, stop=1)=='S',]
+# g.Use your knowledge of handling character strings in R to extract
+# all records from mydataframe that correspond to people whose
+# names start with S. 
+
+mydataframe[substr(mydataframe$person, start = 1, stop=1)=='S',] #correct
--- R-conteudo/Chapter6.R
+++ R-conteudo/Chapter6.R
@@ -1,6 +1,6 @@
 #Special Values, Classes, and Coercion
 
-##Some Special Values
+#### Some Special Values ####
 
 ### Infinity
 
@@ -10,104 +10,163 @@ foo
 bar <- c(3401, Inf, 3.1, -555, Inf, 43)
 bar
 
-baz <- 90000^100
+baz <- 90000 ^ 100
 baz
 
 qux <- c(-42, 565, -Inf, -Inf, Inf, -45632.3)
 qux
+ 
+Inf * -9 #examples of operations that results in infinity
+Inf + 1
+4 * -Inf
+Inf + Inf
+Inf / 23
+23 / Inf
 
-Inf * -9
-Inf+1
-4*-Inf
-Inf+Inf
-Inf/23
-23/Inf
-
--59/0
-59/0
+- 59 / 0
+59 / 0
 
 qux
-is.infinite(qux)
+is.infinite(qux) # verify each element 
 
--Inf<Inf
-Inf>Inf
+- Inf < Inf
+Inf > Inf
 
-qux==Inf
-qux==-Inf
+qux == Inf
+qux == -Inf
 
-### NaN
+### NaN (Not a Number, impossible to express the result)
 
 foo <- NaN
 foo
 
-bar <- c(NaN,54.3,-2,NaN,90094.123,-Inf,55)
+bar <- c(NaN, 54.3,-2, NaN, 90094.123,-Inf, 55)
 bar
 
--Inf+Inf
-Inf/Inf
+- Inf + Inf
+Inf / Inf
 
-0/0
+0 / 0
 
-NaN+1
+NaN + 1
 
 bar
 is.nan(bar)
 is.nan(bar) | is.infinite(bar)
 
-bar[-(which(is.nan(x=bar)|is.infinite(x=bar)))]
+bar[-(which(is.nan(x = bar) | is.infinite(x = bar)))]
 
-# Exercise 6.1
+#### Exercise 6.1 ####
 
-#a.
+# a. Store the following vector: (in book)
+# Then, do the following:
+#   i. Output all elements of foo that, when raised to a power of 75,
+# are NOT infinite.
+# ii. Return the elements of foo, excluding those that result in
+# negative infinity when raised to a power of 75.
 
-foo <- c(13563,-14156,-14319,16981,12921,11979,9568,8833,-12968,8133)
+foo <-
+  c(13563,-14156,-14319,
+    16981,
+    12921,
+    11979,
+    9568,
+    8833,-12968,
+    8133)
 foo
 
-foo[foo^75!=Inf]
+foo[is.finite(foo^75)]
 
-foo[foo^75!=Inf&foo^75!=-Inf]
+foo[-which(foo^75==-Inf)]
 
-#b.
-myvec <- c(77875.40, 27551.45, 23764.30, -36478.88, 
-           -35466.25, -73333.85, 36599.69, -70585.69, 
-           -39803.81, 55976.34, 76694.82, 47032.00)
+# b.Store the following 3 × 4 matrix as the object bar: (in book)
+# Now, do the following:
+#   i. Identify the coordinate-specific indexes of the entries of bar
+# that are NaN when you raise bar to a power of 65 and divide by
+# infinity.
+# ii. Return the values in bar that are NOT NaN when bar is raised
+# to a power of 67 and infinity is added to the result. Confirm
+# this is identical to identifying those values in bar that, when
+# raised to a power of 67, are not equal to negative infinity.
+# iii. Identify those values in bar that are either negative infinity
+# OR finite when you raise bar to a power of 67.
 
-bar <- matrix(myvec, nrow=3, ncol=4)
+myvec <- c(
+  77875.40,
+  27551.45,
+  23764.30,
+  -36478.88,
+  -35466.25,
+  -73333.85,
+  36599.69,
+  -70585.69,
+  -39803.81,
+  55976.34,
+  76694.82,
+  47032.00
+)
+
+bar <- matrix(myvec, nrow = 3,
+              ncol = 4)
 bar
 
-bar^65/Inf
+which(is.nan(bar ^ 65 / Inf), arr.ind = T)
 
-which(is.nan(bar^65/Inf), arr.ind = T)
+bar[!is.nan(bar^67+Inf)]
+bar[bar^67!=-Inf]
 
-bar^67
-which(is.infinite(bar^67), arr.ind = T)
+bar[bar^67==-Inf|is.finite(bar^67)]
 
 ### NA
 
-foo <- c('character','a',NA,'with','string',NA)
+foo <- c('character', 'a', NA, 'with', 'string',
+         NA)
 foo
 
-bar <- factor(c("blue",NA,NA,"blue","green","blue",NA,"red","red",NA,
-                "green"))
+bar <-
+  factor(c(
+    "blue",
+    NA,
+    NA,
+    "blue",
+    "green",
+    "blue",
+    NA,
+    "red",
+    "red",
+    NA,
+    "green"
+  ))
 bar
 
-baz <- matrix(c(1:3,NA,5,6,NA,8,NA),nrow=3,ncol=3)
+baz <- matrix(c(1:3, NA, 5, 6, NA, 8, NA), nrow = 3,
+              ncol = 3)
 baz
 
-qux <- c(NA,5.89,Inf,NA,9.43,-2.35,NaN,2.10,-8.53,-7.58,NA,-4.58,2.01,NaN)
+qux <-
+  c(NA,
+    5.89,
+    Inf,
+    NA,
+    9.43,-2.35,
+    NaN,
+    2.10,-8.53,-7.58,
+    NA,-4.58,
+    2.01,
+    NaN)
 qux
 
 is.na(qux) #flags the NA entries, also, flags NaN entries =/
 
 which(is.nan(qux)) #identify positions whose elements are NaN
 
-which(is.na(qux)&!is.nan(qux)) #JUST NA entries
+which(is.na(qux) & !is.nan(qux)) #JUST NA entries
 
 quux <- na.omit(qux) #omitte NA and NaN from vector
 quux
 
-3+2.1*NA-4 #calculations with NA result in NA
-3*c(1,2,NA,NA,NaN,6)
+3 + 2.1 * NA - 4 #calculations with NA result in NA
+3 * c(1, 2, NA, NA, NaN, 6)
 
 ### NULL
 
@@ -119,8 +178,8 @@ foo #print object dont provide a index
 bar <- NA
 bar
 
-c(2,4,NA,8) #4 elements
-c(2,4,NULL,8)  #3 elements
+c(2, 4, NA, 8) #4 elements
+c(2, 4, NULL, 8)  #3 elements
 
 c(NA, NA, NA)
 c(NULL, NULL, NULL)
@@ -130,11 +189,11 @@ opt.arg <- c('string1', 'string2', 'string3')
 is.na(opt.arg) #check each value
 is.null(opt.arg) #just say if is empty or supplied
 
-NULL+63
-45<=NULL
-NaN-NULL+NA/Inf
+NULL + 63
+45 <= NULL
+NaN - NULL + NA / Inf
 
-foo <- list(member1=c(33,1,5.2,7),member2="NA or NULL?")
+foo <- list(member1 = c(33, 1, 5.2, 7), member2 = "NA or NULL?")
 foo
 foo$member3
 
@@ -145,7 +204,7 @@ foo$member3
 
 #a.
 
-foo <- c(4.3,2.2,NULL,2.4,NaN,3.3,3.1,NULL,3.4,NA)
+foo <- c(4.3, 2.2, NULL, 2.4, NaN, 3.3, 3.1, NULL, 3.4, NA)
 #i. true, correct
 #ii. true, correct
 #iii. false, correct
@@ -154,7 +213,7 @@ foo <- c(4.3,2.2,NULL,2.4,NaN,3.3,3.1,NULL,3.4,NA)
 length(foo)
 which(is.na(foo))
 is.null(foo)
-is.na(x=foo[8])+4/NULL
+is.na(x = foo[8]) + 4 / NULL
 
 #b. don't understand what is alpha and beta called in the question
 
@@ -162,7 +221,10 @@ is.na(x=foo[8])+4/NULL
 
 ### Attributes
 
-foo <- matrix(data=1:9, nrow=3, ncol=3)
+foo <- matrix(data = 1:9,
+              nrow = 3,
+              
+              ncol = 3)
 foo
 
 attributes(foo)
@@ -171,25 +233,33 @@ attr(foo, which = 'dim') #if you know the attribute name
 
 dim(foo) #most commons attributes have their own functions
 
-bar <- matrix(data=1:9,nrow=3,ncol=3,dimnames=list(c("A","B","C"), #give columns
-                                                   c("D","E","F")))#and rows names
+bar <-
+  matrix(
+    data = 1:9,
+    nrow = 3,
+    
+    ncol = 3,
+    dimnames = list(c("A", "B", "C"), #give columns
+                    c("D", "E", "F"))
+  )#and rows names
 bar
 
 attributes(bar)#dimnames are attributes
 
 dimnames(bar)
 
-dimnames(foo) <- list(c("A","B","C"),c("D","E","F"))
+dimnames(foo) <- list(c("A", "B", "C"), c("D", "E", "F"))
 foo
 
 ### Object Class
 #describe and entity in R
 
 num.vec1 <- 1:4
-num.vec2 <- seq(from=1,to=4,length=6)
-char.vec <- c("a","few","strings","here")
-logic.vec <- c(T,F,F,F,T,F,T,T)
-fac.vec <- factor(c("Blue","Blue","Green","Red","Green","Yellow"))
+num.vec2 <- seq(from = 1, to = 4, length = 6)
+char.vec <- c("a", "few", "strings", "here")
+logic.vec <- c(T, F, F, F, T, F, T, T)
+fac.vec <-
+  factor(c("Blue", "Blue", "Green", "Red", "Green", "Yellow"))
 
 class(num.vec1)
 class(num.vec2)
@@ -197,9 +267,11 @@ class(char.vec)
 class(logic.vec)
 class(fac.vec)
 
-ordfac.vec <- factor(x=c("Small","Large","Large","Regular","Small"),
-                     levels=c("Small","Regular","Large"),
-                     ordered=TRUE)
+ordfac.vec <- factor(
+  x = c("Small", "Large", "Large", "Regular", "Small"),
+  levels = c("Small", "Regular", "Large"),
+  ordered = TRUE
+)
 ordfac.vec
 class(ordfac.vec) #multiple class
 
@@ -217,16 +289,16 @@ is.logical(num.vec1)
 
 ### As-Dot Coercion Functions
 
-1:4+c(T,F,F,T)
+1:4 + c(T, F, F, T)
 
 foo <- 34
 bar <- T
 
-paste("Definitely foo: ",foo,"; definitely bar: ",bar,".",sep="")
+paste("Definitely foo: ", foo, "; definitely bar: ", bar, ".", sep = "")
 
-as.numeric(c(T,F,F,T)) #coercion
+as.numeric(c(T, F, F, T)) #coercion
 
-1:4+as.numeric(c(T,F,F,T))
+1:4 + as.numeric(c(T, F, F, T))
 
 
 foo <- 34
@@ -240,30 +312,40 @@ bar.ch
 as.numeric("32.4") #make sense
 as.numeric("g'day mate") #dont make sense
 
-as.logical(c("1","0","1","0","0"))
-as.logical(as.numeric(c("1","0","1","0","0")))
+as.logical(c("1", "0", "1", "0", "0"))
+as.logical(as.numeric(c("1", "0", "1", "0", "0")))
 
-baz <- factor(x=c("male","male","female","male"))
+baz <- factor(x = c("male", "male", "female", "male"))
 baz
 
 as.numeric(baz)
 
-foo <- matrix(data=1:4,nrow=2,ncol=2)
+foo <- matrix(data = 1:4,
+              nrow = 2,
+              ncol = 2)
 foo
 
 as.vector(foo)
 
-bar <- array(data=c(8,1,9,5,5,1,3,4,3,9,8,8),dim=c(2,3,2))
+bar <- array(data = c(8, 1, 9, 5, 5, 1, 3, 4, 3, 9, 8, 8),
+             dim = c(2, 3, 2))
 bar
 
 as.vector(bar)
 
-baz <- list(var1=foo,var2=c(T,F,T),var3=factor(x=c(2,3,4,4,2)))
+baz <- list(var1 = foo,
+            var2 = c(T, F, T),
+            var3 = factor(x = c(2, 3, 4, 4, 2)))
 baz
 
 as.data.frame(baz) #not matching lengths
 
-qux <- list(var1=c(3,4,5,1),var2=c(T,F,T,T),var3=factor(x=c(4,4,2,1)))
+qux <-
+  list(
+    var1 = c(3, 4, 5, 1),
+    var2 = c(T, F, T, T),
+    var3 = factor(x = c(4, 4, 2, 1))
+  )
 qux
 
 as.data.frame(qux)
@@ -272,7 +354,7 @@ as.data.frame(qux)
 
 #a.
 
-foo <- array(data=1:36,dim=c(3,3,4))
+foo <- array(data = 1:36, dim = c(3, 3, 4))
 class(foo)
 bar <- as.vector(foo)
 class(bar)
@@ -280,7 +362,7 @@ baz <- as.character(bar)
 class(baz)
 qux <- as.factor(baz)
 class(qux)
-quux <- bar+c(-0.1,0.1)
+quux <- bar + c(-0.1, 0.1)
 class(quux)
 
 #b.
